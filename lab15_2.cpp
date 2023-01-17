034f057563b6d68c6c4bdf89ee14442496753176
Yotsawat Sukinee <yotsawat2557@gmail.com>
2023-01-17 17:27:20 +0700

    lab15_2 finished

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,39 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void showMatrix(const bool M[][N]){
+	for(int i=0; i<N; i++){
+		for(int j = 0; j<N ;j++){
+			cout << M[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
+
+void inputMatrix(double M[][N]){
+	for(int i=0;i<N;i++){
+		cout << "Row " << i+1 << ": ";
+		for(int j=0;j<N;j++){
+			cin >> M[i][j];
+		}
+	}
+}
+
+void findLocalMax(const double A[][N], bool B[][N]){
+	for(int i=0;i<N;i++){
+		B[0][i] = 0;
+		B[i][0] = 0;
+		B[N-1][i] = 0;
+		B[i][N-1] = 0;
+	}
+	for(int i=1; i<N-1; i++){
+		for(int j=1; j<N-1; j++){
+			if(A[i][j] >= A[i+1][j] && A[i][j] >= A[i-1][j] && A[i][j] >= A[i][j+1] && A[i][j] >= A[i][j-1]){
+				B[i][j] = 1;
+			}else{
+				B[i][j] = 0;
+			}
+		}
+	}
+}
